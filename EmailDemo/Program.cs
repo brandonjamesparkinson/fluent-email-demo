0a366a664b9a4dc9acbfe95d066a0a760db45c14
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2021-02-08 23:42:04 +0000

    + changed to .net 5.0 | added nuget package for fluentemail

--- EmailDemo/Program.cs
+++ EmailDemo/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace EmailDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
